e8ad4466af0dd3002d581dc864b335dc76da2403
RAJABUNISHA <36623634+RAJABUNISHA@users.noreply.github.com>
2018-02-20 10:30:00 +0530

    Create Integers.c

---
+++ Integers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() 
+{
+	int a;
+	printf("Enter the number");
+	scanf("%d",&a);
+	if(a>=1&&a<=100000)
+	{
+		printf("POSITIVE");
+	}
+	else if(a<0)
+	{
+		printf("NEGATIVE");
+	}
+	else if(a=0)
+    {
+    	printf("ZERO");
+    }
+   else if(a>100000)
+   {
+   	printf("BEYOND THE RANGE");
+   }
+   else
+   {
+   	printf(" Invalid key.Enter the Number");
+   }
+   return 0;
+}
+   
+	
